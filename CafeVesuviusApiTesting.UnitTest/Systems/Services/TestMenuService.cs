a47dbc057c2a4f38cac7dffd81fa02d5fac9dd3a
Spillehus <45336031+Spillehus@users.noreply.github.com>
2023-09-18 10:28:53 +0200

    :)

---
+++ CafeVesuviusApiTesting.UnitTest/Systems/Services/TestMenuService.cs
@@ -0,0 +1,23 @@
+﻿using CafeVesuviusApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeVesuviusApiTesting.UnitTest.Systems.Services
+{
+    public class TestMenuService
+    {
+        [Fact]
+        public async Task GetNewestMenu_WhenCalled_InvokesHttpGetRequest()
+        {
+            //Arrange
+             
+            //Act
+
+            //Assert
+
+        }
+    }
+}
