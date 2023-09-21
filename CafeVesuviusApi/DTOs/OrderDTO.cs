871d6d5da578d574d5dfdc6da1e0e372e04e5d0b
Esociety <esocietyhere@gmail.com>
2023-09-21 09:12:37 +0200

    add order name field to dto

--- CafeVesuviusApi/DTOs/OrderDTO.cs
+++ CafeVesuviusApi/DTOs/OrderDTO.cs
@@ -4,6 +4,8 @@ public class OrderDTO
 {
     public long Id { get; set; }
 
+    public string Name { get; set; } = null!;
+    
     public bool Completed { get; set; }
 
     public DateTime? Created { get; set; }
